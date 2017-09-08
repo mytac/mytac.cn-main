2bbe18d6e1ad8dc86512a74936965d9695f68cca
张君卓 <my_tac@126.com>
2017-09-08 16:52:56 +0800

    header的background position y

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import './style.less';
 
+const handleStyle = (positionY = '100%', img = '\'https://image.freepik.com/free-vector/pink-polygonal-background_23-2147495183.jpg\'') => (
+  { positionY, backgroundImage: `url(${img})` }
+);
 
-export default function Header() {
+export default function Header({ positionY, img }) {
   return (
-    <div className="header">
+    <div className="header" style={handleStyle(positionY, img)}>
       <div className="wrapper">
         <div className="logo">
           <span>MyTac</span>
