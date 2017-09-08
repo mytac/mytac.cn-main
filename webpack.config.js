a8803e34a32dfdb2f93929768a8c64c18c2e06a6
张君卓 <my_tac@126.com>
2017-09-08 15:22:17 +0800

    添加semantic-ui，并修改相关配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'todo-app',
-            template: 'template.html',
-        }),
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'todo-app',
+      template: 'template.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [{
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader', // compiles Less to CSS
+        }],
+      },
+      {
+        test: /\.(woff|ttf|eot|svg|jpg|png)\??.*$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react'],
+          },
+        },
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(less|css)$/,
-                use: [{
-                    loader: 'style-loader', // creates style nodes from JS strings
-                }, {
-                    loader: 'css-loader', // translates CSS into CommonJS
-                }, {
-                    loader: 'less-loader', // compiles Less to CSS
-                }],
-            },
-            {
-                test: /\.(woff|ttf|eot|svg|jpg)\??.*$/,
-                loader: 'url-loader',
-            },
-            {
-                test: /\.js[x]?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['react'],
-                    },
-                },
-            },
-        ],
-    },
+  },
 };
