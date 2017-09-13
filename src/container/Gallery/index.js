5236a3b88d770d857eb5d57702a25f7f7d8d8c03
张君卓 <my_tac@126.com>
2017-09-13 16:02:13 +0800

    添加自动滑动函数，优化渲染新图片时没有特效的问题

--- src/container/Gallery/index.js
+++ src/container/Gallery/index.js
@@ -12,9 +12,20 @@ export default class Gallery extends React.Component {
       animateKlass: '',
     };
     this.changeIndex = this.changeIndex.bind(this);
+    this.autoSlide = this.autoSlide.bind(this);
   }
 
-  changeIndex(type) {
+  componentDidMount() {
+    this.autoSlide();
+  }
+
+  autoSlide() {
+    this.timer = setInterval(
+      () => { this.changeIndex('left', 'auto'); }, 5000,
+    );
+  }
+
+  changeIndex(type, caller) {
     /* eslint-disable no-return-assign, no-param-reassign */
     this.setState({
       animateKlass: '',
@@ -35,13 +46,22 @@ export default class Gallery extends React.Component {
           animateKlass: 'slideInRight',
         };
       });
-    }, 0);
+    }, 50);
+    clearInterval(this.timer);
+
+    if (caller) {
+      this.autoSlide();
+    } else {
+      setTimeout(() => {
+        this.autoSlide();
+      }, 2000);
+    }
   }
+
 
   render() {
     const { imgArray } = this.props;
     const { index, animateKlass } = this.state;
-
     return (
       <div className="gallery">
         <div alt="" className={`img animated ${animateKlass}`} style={{ backgroundImage: `url(${imgArray[index]})` }} />
