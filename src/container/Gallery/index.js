e36bc95093f05fdd779b5cacebc0b7c5aa78cd38
张君卓 <my_tac@126.com>
2017-09-13 11:52:42 +0800

    图片数组循环

--- src/container/Gallery/index.js
+++ src/container/Gallery/index.js
@@ -9,18 +9,31 @@ export default class Gallery extends React.Component {
     this.state = {
       index: 0,
     };
+    this.changeIndex = this.changeIndex.bind(this);
+  }
+
+  changeIndex(type) {
+    /* eslint-disable no-return-assign, no-param-reassign */
+    this.setState((prevState, props) => {
+      let prev = prevState.index;
+      const len = props.imgArray.length;
+      if (type === 'left') {
+        return { index: prev === 0 ? len - 1 : prev -= 1 };
+      }
+      return { index: prev === len - 1 ? 0 : prev += 1 };
+    });
   }
 
   render() {
     const { imgArray } = this.props;
     const { index } = this.state;
-
+    
     return (
       <div className="gallery">
         <img src={imgArray[index]} alt="" />
         <div className="btn-wrapper">
-          <LeftBtn />
-          <RightBtn />
+          <LeftBtn event={() => this.changeIndex('left')} />
+          <RightBtn event={() => this.changeIndex('right')} />
         </div>
       </div>
     );
