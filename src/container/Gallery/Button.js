7ce1375e2d4ee956345a2eec16bd9354f5123481
张君卓 <my_tac@126.com>
2017-09-13 10:58:27 +0800

    gallery左右按钮样式

--- src/container/Gallery/Button.js
+++ src/container/Gallery/Button.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 export function LeftBtn({ event }) {
   return (
-    <div className="gallery-button left" onClick={event} />
+    <div className="gallery-button" onClick={event} >
+      <i className="icon-left-arrow" />
+    </div>
   );
 }
 
 export function RightBtn({ event }) {
   return (
-    <div className="gallery-button right" onClick={event} />
+    <div className="gallery-button" onClick={event} >
+      <i className="icon-right-arrow" />
+    </div>
   );
 }
