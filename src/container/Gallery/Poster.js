3cf1fd2d2b48791f521a4361e7828b9a06ba67b4
张君卓 <my_tac@126.com>
2017-09-13 17:11:43 +0800

    修正poster文字颜色被滤镜污染的问题

--- src/container/Gallery/Poster.js
+++ src/container/Gallery/Poster.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function Poster({ animateKlass, imgUrl }) {
   return (
-    <div alt="" className={`poster animated ${animateKlass}`} style={{ backgroundImage: `url(${imgUrl})` }} >
+    <div alt="" className={`poster animated ${animateKlass}`} >
+      <div className="bg" style={{ backgroundImage: `url(${imgUrl})` }} />
       <div className="description">
         <p className="title">Let Me In absolute absolute ute absoluteute absolute</p>
         <p className="rm">- Read More - </p>
