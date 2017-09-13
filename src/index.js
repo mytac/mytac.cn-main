8eeb87aedfb386d6dd4ea498260b84183ebc8d72
张君卓 <my_tac@126.com>
2017-09-13 17:54:16 +0800

    css删错了

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import 'semantic-ui-css/semantic.min.css';
+
 import './reset.less';
 import './style.less';
 
@@ -12,7 +14,6 @@ const tempPics = [
   'http://img06.tooopen.com/images/20170514/tooopen_sy_210122159348.jpg',
   'http://desk.fd.zol-img.com.cn/t_s960x600c5/g3/M03/0D/03/Cg-4V1S_EOWIMyUCAAhG5zFfIHUAATsVQNFKM0ACEb_770.jpg',
 ];
-
 
 render(
   <div>
