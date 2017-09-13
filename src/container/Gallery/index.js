d55fba8ce30a65598f309361c468d46e70d06c3b
张君卓 <my_tac@126.com>
2017-09-13 14:33:04 +0800

    加上左右按钮点击事件动画

--- src/container/Gallery/index.js
+++ src/container/Gallery/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './style.less';
+import 'animate.css';
 
+import './style.less';
 import { LeftBtn, RightBtn } from './Button';
 
 export default class Gallery extends React.Component {
@@ -8,29 +9,42 @@ export default class Gallery extends React.Component {
     super(props);
     this.state = {
       index: 0,
+      animateKlass: '',
     };
     this.changeIndex = this.changeIndex.bind(this);
   }
 
   changeIndex(type) {
     /* eslint-disable no-return-assign, no-param-reassign */
-    this.setState((prevState, props) => {
-      let prev = prevState.index;
-      const len = props.imgArray.length;
-      if (type === 'left') {
-        return { index: prev === 0 ? len - 1 : prev -= 1 };
-      }
-      return { index: prev === len - 1 ? 0 : prev += 1 };
+    this.setState({
+      animateKlass: '',
     });
+
+    setTimeout(() => {
+      this.setState((prevState, props) => {
+        let prev = prevState.index;
+        const len = props.imgArray.length;
+        if (type === 'left') {
+          return {
+            index: prev === 0 ? len - 1 : prev -= 1,
+            animateKlass: 'slideInLeft',
+          };
+        }
+        return {
+          index: prev === len - 1 ? 0 : prev += 1,
+          animateKlass: 'slideInRight',
+        };
+      });
+    }, 0);
   }
 
   render() {
     const { imgArray } = this.props;
-    const { index } = this.state;
-    
+    const { index, animateKlass } = this.state;
+
     return (
       <div className="gallery">
-        <img src={imgArray[index]} alt="" />
+        <div alt="" className={`img animated ${animateKlass}`} style={{ backgroundImage: `url(${imgArray[index]})` }} />
         <div className="btn-wrapper">
           <LeftBtn event={() => this.changeIndex('left')} />
           <RightBtn event={() => this.changeIndex('right')} />
