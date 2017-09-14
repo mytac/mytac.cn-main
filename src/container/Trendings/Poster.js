0024b5423c318d11f457eab0341fdcf9cabad249
张君卓 <my_tac@126.com>
2017-09-14 15:34:11 +0800

    给trending加上文字描述

--- src/container/Trendings/Poster.js
+++ src/container/Trendings/Poster.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function Poster({ imgUrl }) {
   return (
-    <div className="poster" style={{ backgroundImage: `url(${imgUrl})` }} >
-      <div className="filter-layout" />
+    <div className="poster">
+      <div className="bg" style={{ backgroundImage: `url(${imgUrl})` }} >
+        <div className="filter-layout" />
+      </div>
+      <div className="description">
+        <p className="title">Let Me In absolute absolute ute absoluteute absolute</p>
+        <p className="rm">- Read More - </p>
+      </div>
     </div>
   );
 }
