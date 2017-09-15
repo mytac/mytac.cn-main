72645959ce975ce6a0a5c2e523229c7bdb20361b
张君卓 <my_tac@126.com>
2017-09-15 16:09:56 +0800

    storypage组件构建完成

--- src/component/StoryPage/index.js
+++ src/component/StoryPage/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReadMoreBtn from '../../component/ReadMoreBtn';
 import './style.less';
 
 export default function StoryPage({ title, img, content }) {
   return (
     <div className="story-page">
-      <div className="title" >
-        {title}
+      <div className="title-wrapper" >
+        <p className="title">{title}</p>
+        <p className="date">April 26,2017</p>
       </div>
       <div className="pic">
         <img src={img} alt="" />
       </div>
       <div className="content">
-        {content}
+        <p>{content}</p>
+        <p><ReadMoreBtn text="read more" /></p>
       </div>
     </div>
   );
--- src/container/Featured/index.js
+++ src/container/Featured/index.js
@@ -4,6 +4,8 @@ import Title from '../../component/Title';
 import StoryPage from '../../component/StoryPage';
 import './style.less';
 
+const temp = 'Milk the cow kitty scratches couch bad kitty. Chase red laser dot please stop looking at your phone and pet me. I could pee on this if i had the energy sun bathe eat the fat cats food. Knock dish off table head butt cant eat out of my own dish. Eat all the power cords spill litter box, scratch at owner, destroy all furniture, especially couch for scratch at the door then walk awaychew iPad power cord dream about hunting birds. Caticus cuteicus kitty! kitty!, so give attitude, and scream for no reason at 4 am or claws in your leg for stand in front of the computer screen';
+
 export default class Featured extends React.Component {
   constructor(props) {
     super(props);
@@ -14,8 +16,8 @@ export default class Featured extends React.Component {
       <div className="featured">
         <Title title="FEATURED" />
         <div className="pages-wrapper">
-          <StoryPage content="this is content" img=" " title="this is title" />
-          <StoryPage content="this is content" img=" " title="this is title" />
+          <StoryPage content={temp} img="https://image.freepik.com/free-vector/pink-polygonal-background_23-2147495183.jpg" title="this is title" />
+          <StoryPage content={temp} img=" " title="this is title" />
         </div>
       </div>
     );
