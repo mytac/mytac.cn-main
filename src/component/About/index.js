779ea803eaf91e4c7b6b95a47203c20728bbdc48
张君卓 <my_tac@126.com>
2017-09-18 10:59:39 +0800

    Footer组件初始化

---
+++ src/component/About/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Title from '../Title';
+
+export default function About() {
+  return (
+    <Title title="ABOUT" />
+  );
+}
---
+++ src/component/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.less';
+
+import About from '../../component/About';
+
+export default function Footer() {
+  return (
+    <div className="footered">
+        11
+      <About />
+    </div>
+  );
+}
