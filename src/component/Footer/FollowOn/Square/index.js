020cb0fc2ab9241afd1053cdfebe66a5eee2356c
张君卓 <my_tac@126.com>
2017-09-18 14:43:12 +0800

    FollowOn组件完成

--- src/component/Footer/FollowOn/Square/index.js
+++ src/component/Footer/FollowOn/Square/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.less';
 
-export default function SquareButton({ icon }) {
+export default function SquareButton({ icon, title }) {
   return (
-    <div className="square-button">
+    <a className="square-button" title={title}>
       <i className={icon} />
-    </div>
+    </a>
   );
 }
 
 SquareButton.propTypes = {
   icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
