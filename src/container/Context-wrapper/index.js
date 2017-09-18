509aab9af5122b779e9d74d4ca8ccc21768d5380
张君卓 <my_tac@126.com>
2017-09-18 17:31:19 +0800

    给Context-wrapper绑定ScrollDetector

--- src/container/Context-wrapper/index.js
+++ src/container/Context-wrapper/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScrollDetector from '../ScrollDetector';
 
 import './style.less';
 import LittlePage from '../../component/LittlePage';
@@ -7,12 +8,25 @@ import LittlePage from '../../component/LittlePage';
 export default class ContextWrapper extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isShow: false,
+
+    };
+    this.getChildState = this.getChildState.bind(this);
+  }
+
+  getChildState(state) {
+    this.setState({
+      isShow: state,
+    });
   }
 
   render() {
     const { imgArr } = this.props;
+    const { isShow } = this.state;
     return (
-      <div className="context-wrapper">
+      <div className={`context-wrapper animated ${isShow ? 'fadeIn' : 'fadeOut'}`}>
+        <ScrollDetector getChildState={this.getChildState} />
         {imgArr.map((imgUrl, index) => <LittlePage img={imgUrl} key={`${index}-${imgUrl}`} />)}
       </div>
     );
