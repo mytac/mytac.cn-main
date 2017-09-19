a9b881a8bfe7096e13c6ed6f1429128452ae0375
张君卓 <my_tac@126.com>
2017-09-19 16:16:35 +0800

    react-router初始化、绑定到gallery组件上

---
+++ src/container/DetailPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  Link,
+} from 'react-router';
+
+export default function DetailPage({ id }) {
+  return (
+    <div>DetailPage:{id}</div>
+  );
+}
--- src/container/Gallery/index.js
+++ src/container/Gallery/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hammer from 'react-hammerjs';
 import 'animate.css';
 
@@ -78,7 +79,7 @@ export default class Gallery extends React.Component {
           onSwipeLeft={() => this.changeIndex('left')}
           onSwipeRight={() => this.changeIndex('right')}
         >
-          <Poster animateKlass={animateKlass} imgUrl={imgArray[index]} />
+          <Link to="/123"><Poster animateKlass={animateKlass} imgUrl={imgArray[index]} /></Link>
         </Hammer>
         <div className="btn-wrapper">
           <LeftBtn event={() => this.changeIndex('left')} />
