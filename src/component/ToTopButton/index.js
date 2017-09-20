af8e3a8833e5cb96903858239b1f9f2960743e87
张君卓 <my_tac@126.com>
2017-09-20 14:10:04 +0800

    根据滚轮高度判断是否显示toTopButton

--- src/component/ToTopButton/index.js
+++ src/component/ToTopButton/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './style.less';
 
-export default function ToTopButton() {
-  return (
-    <div className="to-top-button">
-      <i className="fa fa-chevron-up" aria-hidden="true" />
-    </div>
-  );
+export default class ToTopButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShow: true,
+    };
+
+    this.viewHeight = document.documentElement.clientHeight;
+    this.getScrollHeight = this.getScrollHeight.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.getScrollHeight, false);
+    this.focusTextInput.focus();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.getScrollHeight, false);
+  }
+
+  // 根据滚轮高度判断是否显示toTopButton
+  getScrollHeight() {
+    const { top } = this.focusTextInput.parentNode.getBoundingClientRect();
+    this.setState({
+      isShow: Math.abs(top) > this.viewHeight,
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="to-top-button" ref={(pos) => { this.focusTextInput = pos; }} style={{ display: !this.state.isShow && 'none' }}>
+        <i className="fa fa-chevron-up" aria-hidden="true" />
+      </div>
+    );
+  }
 }
